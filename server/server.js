e5b5709d2d5bdefee91a8fd49ed86878810c84b3
Maxwell Volz <maxwellvolz@gmail.com>
2023-04-07 15:43:32 -0700

    moved client to server public, resolved socket error

--- server/server.js
+++ server/server.js
@@ -2,7 +2,16 @@ const express = require("express");
 const app = express();
 const http = require("http").createServer(app);
 const io = require("socket.io")(http);
-const { RTCPeerConnection, RTCSessionDescription } = require("wrtc");
+const cors = require("cors");
+const { RTCPeerConnection, RTCSessionDescription, RTCIceCandidate } = require("wrtc");
+
+// Allow requests from all origins
+app.use(cors());
+
+// Specific origins
+// app.use(cors({
+//   origin: "http://localhost:58703"
+// }));
 
 // Serve static files from the "public" directory
 app.use(express.static("public"));
@@ -13,61 +22,31 @@ const signaling = io.of("/signaling");
 // Handle new socket connections
 signaling.on("connection", (socket) => {
     console.log(`New signaling connection: ${socket.id}`);
-
-    // Create a new RTCPeerConnection object for this connection
     const pc = new RTCPeerConnection();
-
-    // Add event handlers for ICE candidates and incoming data channels
     pc.onicecandidate = (event) => {
         if (event.candidate) {
             socket.emit("candidate", event.candidate);
         }
     };
-
-    pc.ondatachannel = (event) => {
-        console.log(`New data channel: ${event.channel.label}`);
-        const channel = event.channel;
-
-        // Handle incoming data messages
-        channel.onmessage = (event) => {
-            console.log(`Received message from ${socket.id}: ${event.data}`);
-
-            // Broadcast the message to all connected clients (excluding the sender)
-            channel.send(`[${socket.id}]: ${event.data}`);
-        };
-    };
-
-    // Handle incoming signaling messages
     socket.on("offer", async (description) => {
+        console.log("damn")
         console.log(`Received offer from ${socket.id}`);
-
         await pc.setRemoteDescription(new RTCSessionDescription(description));
-
         const answer = await pc.createAnswer();
-
         await pc.setLocalDescription(answer);
-
         socket.emit("answer", answer);
+        console.log("Answer sent: ", answer);
     });
-
     socket.on("answer", async (description) => {
         console.log(`Received answer from ${socket.id}`);
-
         await pc.setRemoteDescription(new RTCSessionDescription(description));
     });
-
-
     socket.on("candidate", async (candidate) => {
         console.log(`Received ICE candidate from ${socket.id}`);
-
         await pc.addIceCandidate(new RTCIceCandidate(candidate));
     });
-
-    // Handle socket disconnections
-    socket.on("disconnect", () => {
-        console.log(`Disconnected: ${socket.id}`);
-    });
 });
+
 
 // Start the server and listen for incoming connections
 http.listen(3000, () => {
